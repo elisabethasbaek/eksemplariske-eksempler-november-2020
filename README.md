494201f17d2d71798d779053093f5c2721020eee
elisabeth asbæk jeppesen <elisabeth.asbaek@gmail.com>
2020-11-12 10:59:57 +0100

    so far so good

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# boilerplate
+## NPM + Gulp
+
+!! Sørg for at npm er installeret på computeren
+
+**``npm init``**
+(Husk: MIT) // Dette opretter _package.json_
+
+**``npm i gulp``**
+Dette opretter _package-lock.json_
+
+I package.json tilføj under _"scripts"_:
+**``"start": "gulp",``**
+
+Skriv **``gulpfile.js``** filen
+
+Hvis installation af gulp-filer fejler (på Windows) i terminalen i VS Code, så åben mappen dit projekt ligger i i stifinderen, klik på _fil_ oppe i toppen, og vælg _PowerShell_
+
+Hvad du gerne vil have i din _gulpfile.js_:
+
+Kopier: 
+**``npm i gulp gulp-babel gulp-clean-css gulp-concat gulp-connect gulp-htmlmin gulp-image-resize gulp-sass gulp-sourcemaps``**
+
+```javascript
+"dependencies": {
+    "@babel/core": "^7.12.3",
+    "@babel/preset-env": "^7.12.1",
+    "gulp": "^4.0.2",
+    "gulp-babel": "^8.0.0",
+    "gulp-clean-css": "^4.3.0",
+    "gulp-concat": "^2.6.1",
+    "gulp-connect": "^5.7.0",
+    "gulp-htmlmin": "^5.0.1",
+    "gulp-image-resize": "^0.13.1",
+    "gulp-sass": "^4.1.0",
+    "gulp-sourcemaps": "^2.6.5"
+  }
+```
+
+**``npm start``** for at starte gulp // 
+**``ctrl + C``** for at afslutte gulp
--- gulpfile.js
+++ gulpfile.js
@@ -2,63 +2,69 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var connect = require("gulp-connect");
 var sourcemap = require ("gulp-sourcemaps");
+var htmlmin = require("gulp-htmlmin");
 var cleanCSS = require("gulp-clean-css");
 var resize = require("gulp-image-resize");
 var babel = require("gulp-babel");
-var concat = require("gulp-concat");  //alle javascriptfiler i src-mappen sættes sammen til én fil, så man behøver ikke import/export
+var concat = require("gulp-concat"); //alle javascriptfiler i src-mappen sættes sammen til én fil, så man behøver ikke import/export
 
 function processHTML(){
-    return gulp.src("src/html/**/*.html")
-        .pipe(gulp.dest("dist/"))
+	return gulp.src("./src/html/**/*.html") //hvilke filer vi skal have fat i
+		.pipe(sourcemap.init())
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(sourcemap.write(".")) //sourcemap mappen skal lægges i samme mappe som vores destination fil
+        .pipe(gulp.dest("./dist/")) //hvilken mappe filerne ender i
         .pipe(connect.reload());
 }
 
 function processSass(){
-    return gulp.src("src/scss/**/*.scss")
+    return gulp.src("./src/scss/**/*.scss")
+		.pipe(sourcemap.init())
         .pipe(sass())
-        .pipe(cleanCSS({ compatibility: "ie9" }))
-        .pipe(gulp.dest("dist/css"))
+		.pipe(cleanCSS({ compatibility: "ie9" }))
+		.pipe(sourcemap.write(".")) //sourcemap mappen skal lægges i samme mappe som vores destination fil
+        .pipe(gulp.dest("./dist/css"))
         .pipe(connect.reload());
 }
 
 function processJS(){
-	return gulp.src("src/js/**/*.js")
+	return gulp.src("./src/js/**/*.js")
 	.pipe(sourcemap.init())
 	.pipe(babel({
 		presets: ["@babel/env"]
 	}))
 	.pipe(concat("app.js"))
-	.pipe(sourcemap.write("."))
-	.pipe(gulp.dest("dist/js"))
+	.pipe(sourcemap.write(".")) //sourcemap mappen skal lægges i samme mappe som vores destination fil
+	.pipe(gulp.dest("./dist/js"))
 	.pipe(connect.reload());
 }
 
 function processImages(){
-	return gulp.src("src/images/**/*")
+	return gulp.src("./src/images/**/*")
 		.pipe(resize({
 			width: 100,
 			height: 0,
 			crop: false,
       		upscale: false
 		}))
-        .pipe(gulp.dest("dist/images"))
+        .pipe(gulp.dest("./dist/images"))
 		.pipe(connect.reload());
 }
 
-function watchEmAll(){
-    gulp.watch("src/html/**/*.html",
+function watchEmAll(){ //hvis der er ændringer i filerne i de respektive mapper, så tygger gulp dem igennem
+    gulp.watch("./src/html/**/*.html",
     { ignoreInitial: false },
     processHTML);
 
-    gulp.watch("src/scss/**/*.scss",
+    gulp.watch("./src/scss/**/*.scss",
 	{ ignoreInitial: false },
     processSass);
     
-    gulp.watch("src/js/**/*.js",
+    gulp.watch("./src/js/**/*.js",
 	{ ignoreInitial: false },
 	processJS);
 
-    gulp.watch("src/images/**/*",
+    gulp.watch("./src/images/**/*",
 	{ ignoreInitial: false },
 	processImages);
 }
