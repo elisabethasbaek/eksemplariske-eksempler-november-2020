26862347dbb61ed5e26405e57aa1e7be6f5c3f3d
elisabeth asbæk jeppesen <elisabeth.asbaek@gmail.com>
2020-11-13 13:03:27 +0100

    jkfh

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,9 +1,23 @@
-fetch("https://icanhazdadjoke.com")
+fetch("https://icanhazdadjoke.com", {
+    headers: {
+        "accept": "application/json"
+    }
+ })
     .then(function(response){
-        console.log(response)
         if(response.status == 200){
             return response.json();
         } else{
-            throw new Error("Server do a doodoo");
+            throw new Error("Server do a doodoo, boo boo :(");
         }
+    })
+    .then(function(data){
+        var h2 = document.createElement("h2");
+        h2.innerText = data.joke;
+        document.body.appendChild(h2);
+    })
+    .catch(function(error){
+        console.error(error);
+        var p = document.createElement("p");
+        p.innerText = "Server do a doodoo, boo boo :(";
+        document.body.appendChild(p);
     })
